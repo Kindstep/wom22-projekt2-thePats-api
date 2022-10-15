16dda5f159f1916ff4042880e280fef688b6502e
Kindstep <kindstep@arcada.fi>
2022-10-15 13:27:48 +0300

    fixed DELETE/service

--- routes/routes.service.js
+++ routes/routes.service.js
@@ -97,7 +97,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
-  await Order.destroy({
+  await Service.destroy({
     where: { id: id }
   })
     .then(num => {
